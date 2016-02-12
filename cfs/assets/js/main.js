9c42c96313491b14e7335d1ca2859ea31f360c4d
Wade Nairn <Wade@Wades-MacBook-Pro.local>
2016-02-12 12:04:28 +1100

    fixed bugs in the header and scrolling. Ready for review and/or content

--- cfs/assets/js/main.js
+++ cfs/assets/js/main.js
@@ -1,5 +1,17 @@
-// jQuery to open the links in their specific tabs.
 
+// Make the tab bar stick to the top of the page when it hits it.
+$(window).scroll(function(){
+  	var sticky = $('.mdl-layout__tab-bar'),
+        scroll = $(window).scrollTop();
+
+  	if (scroll >= 180) {
+  		sticky.addClass('fixed');
+  	}else{
+  		sticky.removeClass('fixed');
+  	} 
+});
+
+// jQuery to open the links in their specific tabs.
 $(function () {
     $(".mdl-tabs__panel").hide().first().show();
     $(".mdl-layout__tab-bar li:first").addClass("is-active");
@@ -7,23 +19,22 @@ $(function () {
     $(".mdl-layout__tab-bar a").on('click', function (e) {
         e.preventDefault();
         $(this).closest('.mdl-tabs__tab').addClass("is-active").siblings().removeClass("is-active");
-        $($(this).attr('href')).show().siblings('.mdl-tabs__panel').hide(); 
+        $($(this).attr('href')).show().siblings('.mdl-tabs__panel').hide();
+        $("body").animate({ scrollTop: $('.mdl-layout__tab-bar').offset() });
     });
 
     var hash = $.trim( window.location.hash );
     if (hash) $('.mdl-layout__tab-bar a[href$="'+hash+'"]').trigger('click');
+  
 });
 
 
 
-// Make the tab bar stick to the top of the page when it hits it.
-// $(window).scroll(function(){
-//   	var sticky = $('.mdl-layout__tab-bar'),
-//         scroll = $(window).scrollTop();
+// Open new tabs at the tab top of the tab
+// $(function () {
+// 	$(".mdl-layout__tab-bar a").on('click', function(){
+// 		// $(document).scrollTop(170);
+// 		$("body").animate({ scrollTop: $('.mdl-layout__tab-bar').offset() });
+// 	});
 
-//   	if (scroll >= 185) {
-//   		sticky.addClass('fixed');
-//   	}else{
-//   		sticky.removeClass('fixed');
-//   	} 
 // });

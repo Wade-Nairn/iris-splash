9d433cacd48e6aa757280a0820badc05833595ae
Wade Nairn <wadenairn@gmail.com>
2016-02-12 16:59:15 +1100

    solved js bugs and crossbroswer bugs

--- cfs/assets/js/main.js
+++ cfs/assets/js/main.js
@@ -1,40 +1,36 @@
+$(document).ready(function(){
+    $(this).scrollTop(0);
+  // Make the tab bar stick to the top of the page when it hits it.
 
-// Make the tab bar stick to the top of the page when it hits it.
-$(window).scroll(function(){
-  	var sticky = $('.mdl-layout__tab-bar'),
-        scroll = $(window).scrollTop();
+  $(function(){
+          var stickyHeader = $('.mdl-layout__tab-bar').offset().top;
+            
+          $(window).scroll(function(){
+                  if( $(window).scrollTop() > stickyHeader ) {
+                          $('.mdl-layout__tab-bar').css({position: 'fixed', top: '0px'});
+                          $('.mdl-grid').css('margin-top', '2em');
+                  } else {
+                          $('.mdl-layout__tab-bar').css({position: 'static', top: '0px'});
+                          $('.mdl-grid').css('margin-top', '0');
+                  }
+          });
+  });
 
-  	if (scroll >= 180) {
-  		sticky.addClass('fixed');
-  	}else{
-  		sticky.removeClass('fixed');
-  	} 
+  // jQuery to open the links in their specific tabs.
+  $(function () {
+      $(".mdl-tabs__panel").hide().first().show();
+      $(".mdl-layout__tab-bar li:first").addClass("is-active");
+
+      $(".mdl-layout__tab-bar a").click( function (e) {
+          e.preventDefault();
+          $(this).scrollTop(0);
+          $(this).closest('.mdl-tabs__tab').addClass("is-active").siblings().removeClass("is-active");
+          $($(this).attr('href')).show().siblings('.mdl-tabs__panel').hide();
+          $("body, html").animate({ scrollTop: $(this) });
+      });
+
+      var hash = $.trim( window.location.hash );
+      if (hash) $('.mdl-layout__tab-bar a[href$="'+hash+'"]').trigger('click');
+    
+  });
 });
-
-// jQuery to open the links in their specific tabs.
-$(function () {
-    $(".mdl-tabs__panel").hide().first().show();
-    $(".mdl-layout__tab-bar li:first").addClass("is-active");
-
-    $(".mdl-layout__tab-bar a").on('click', function (e) {
-        e.preventDefault();
-        $(this).closest('.mdl-tabs__tab').addClass("is-active").siblings().removeClass("is-active");
-        $($(this).attr('href')).show().siblings('.mdl-tabs__panel').hide();
-        $("body").animate({ scrollTop: $('.mdl-layout__tab-bar').offset() });
-    });
-
-    var hash = $.trim( window.location.hash );
-    if (hash) $('.mdl-layout__tab-bar a[href$="'+hash+'"]').trigger('click');
-  
-});
-
-
-
-// Open new tabs at the tab top of the tab
-// $(function () {
-// 	$(".mdl-layout__tab-bar a").on('click', function(){
-// 		// $(document).scrollTop(170);
-// 		$("body").animate({ scrollTop: $('.mdl-layout__tab-bar').offset() });
-// 	});
-
-// });

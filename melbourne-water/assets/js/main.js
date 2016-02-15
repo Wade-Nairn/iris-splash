afb145094755a0c8e0d012daa891fff29823f443
Wade Nairn <wadenairn@gmail.com>
2016-02-15 11:57:50 +1100

    working on melbourne-water linking js, and layouts

---
+++ melbourne-water/assets/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $(this).scrollTop(0);
+
+  // jQuery to open the links in their specific tabs.
+  $(function () {
+      $(".mdl-layout__tab-panel").hide().first().show();
+      $(".mdl-layout__tab-bar a:first").addClass("is-active");
+
+      $(".mdl-layout__tab-bar > a").click( function (e) {
+          e.preventDefault();
+          $("body, html").animate({ scrollTop: $(this) });
+          $(this).closest('.mdl-layout__tab-bar > a').addClass("is-active").siblings().removeClass("is-active");
+          $($(this).attr('href')).show().siblings('.mdl-layout__tab-panel').hide();
+          // $("body, html").animate({ scrollTop: $(this) });
+      });
+
+      var hash = $.trim( window.location.hash );
+      if (hash) $('.mdl-layout__tab-bar a[href$="'+hash+'"]').trigger('click');
+    
+  });
+});
